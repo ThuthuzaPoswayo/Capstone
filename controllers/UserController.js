53f06f49a71330394e056738c970314c85151139
ThuthuzaPoswayo <thuthuzaposwayo@gmail.com>
2024-09-13 11:50:01 +0200

    push

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -28,4 +28,39 @@ userRouter.post('/login', (req, res) => {
     user.login(req, res);
 });
 
+// Import bcrypt to hash/compare passwords
+const bcrypt = require('bcrypt');
+
+// Login route
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await db.query('SELECT * FROM Users WHERE email = ?', [email]);
+
+    if (user.length === 0) {
+      // User does not exist
+      return res.json({ msg: 'User not found. Please sign up.', result: false });
+    }
+
+    // Compare hashed password
+    const isPasswordValid = await bcrypt.compare(password, user[0].password);
+
+    if (!isPasswordValid) {
+      // Password is incorrect
+      return res.json({ msg: 'Incorrect password. Please try again.', result: false });
+    }
+
+    // If password is correct, generate a token
+    const token = jwt.sign({ userId: user[0].user_id, role: user[0].role }, SECRET_KEY, { expiresIn: '1h' });
+
+    // Respond with success, token, and user data
+    res.json({ msg: 'Login successful', result: true, token });
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error', result: false });
+  }
+});
+
+
 export { userRouter };
